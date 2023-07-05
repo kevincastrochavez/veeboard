c70bb26c3c5d1d09f6a28eefd6852bfef3517528
Kevin Castro <cas20057@byui.edu>
2023-07-05 16:07:25 -0600

    added overlay to menu

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 const menuIcon = document.getElementById('menuIcon');
 const menu = document.querySelector('.header__right');
 const nav = document.querySelector('nav');
+const header = document.querySelector('header');
 let openedMenu = false;
 
 menuIcon.addEventListener('click', function () {
   openedMenu = !openedMenu;
-  menuIcon.src = openedMenu ? './assets/close.svg' : './assets/menu.svg';
 
-  openedMenu ? nav.classList.add('showMenu') : nav.classList.remove('showMenu');
+  if (openedMenu) {
+    menuIcon.src = './assets/close.svg';
+    nav.classList.add('showMenu');
+
+    const overlay = document.createElement('div');
+    overlay.classList.add('overlayMenu');
+    header.appendChild(overlay);
+
+    overlay.addEventListener('click', function () {
+      openedMenu = !openedMenu;
+      menuIcon.src = './assets/menu.svg';
+      nav.classList.remove('showMenu');
+
+      overlay.remove();
+    });
+  } else {
+    menuIcon.src = './assets/menu.svg';
+    nav.classList.remove('showMenu');
+
+    const overlay = document.querySelector('.overlayMenu');
+    overlay.remove();
+  }
 });
