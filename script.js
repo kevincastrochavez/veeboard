75482a281432b15a6e37a8cd8d3bdf6cc7990a2b
Kevin Castro <cas20057@byui.edu>
2023-07-05 14:53:46 -0600

    added initial javascript for the menu

---
+++ script.js
@@ -0,0 +1,7 @@
+const menuIcon = document.getElementById('menuIcon');
+const menu = document.querySelector('.header__right');
+
+menuIcon.addEventListener('click', function () {
+  console.log(menu);
+  menuIcon.src = 'https';
+});
