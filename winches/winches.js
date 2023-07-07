232bc1830a806b97a47122cdd4fe5fc220c6547a
Kevin Castro <cas20057@byui.edu>
2023-07-06 22:42:27 -0600

    included javascript for when clicking search input

---
+++ winches/winches.js
@@ -0,0 +1,9 @@
+const searchInput = document.getElementById('searchInput');
+const searchIcon = document.getElementById('searchIcon');
+
+searchIcon.addEventListener('click', () => {
+  if (searchInput.value.trim() !== '') {
+    // TODO: Include logic for search
+    console.log('Something');
+  }
+});
