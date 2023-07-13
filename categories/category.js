c5fe9126af2535c38e717c78b94a608176d320ef
Kevin Castro <cas20057@byui.edu>
2023-07-13 16:08:20 -0600

    injected dynamic products from database into page

--- categories/category.js
+++ categories/category.js
@@ -29,20 +29,39 @@ const categoriesText = [
   },
 ];
 
+// Getting current page category from params
 const currentCategory = getCategoryParam('category');
+
+// Getting heading and subheadings according to category
 const { heading, subheading } = categoriesText.find(
   (category) => category.categoryPath === currentCategory
 );
 
-const productCategoryTemplate = `
-  <a href="/winches/230049" class="winches__products-product">
-    <img src="../assets/winch.jpg" alt="" />
-    <div class="productInfo">
-      <h2>417CE</h2>
-      <p>4" x 27' Winch Strap with 18" chain</p>
-    </div>
-  </a>
-`;
+// Product template array from current page category
+const productCategoryTemplate = (categoryInventory) => {
+  const result = categoryInventory.map((product) => {
+    return `<a href="/winches/230049" class="winches__products-product">
+      <img src="${product.imageUrl}" alt="${product.description}" />
+      <div class="productInfo">
+        <h2>${product.partNumber}</h2>
+        <p>"${product.description}</p>
+      </div>
+    </a>
+  `;
+  });
+
+  return result;
+};
+
+// Fetching products from 'database' according to category
+const productsResponse = await fetch('../allProducts.json');
+const productsData = await productsResponse.json();
+const categoryProducts = productsData.filter(
+  (product) => product.category === currentCategory
+);
+
+// HTML ready to be injected with products from such category
+const productsHtmlReady = productCategoryTemplate(categoryProducts).join(' ');
 
 const categoryHtmlTemplate = `
   <h1>${heading}</h1>
@@ -66,7 +85,11 @@ const categoryHtmlTemplate = `
   </div>
 `;
 
-console.log(categoryHtmlTemplate);
+const categoryContainer = document.getElementById('products');
+categoryContainer.innerHTML = categoryHtmlTemplate;
+
+const productsContainer = document.querySelector('.winches__products');
+productsContainer.innerHTML = productsHtmlReady;
 
 // const searchInput = document.getElementById('searchInput');
 // const searchIcon = document.getElementById('searchIcon');
