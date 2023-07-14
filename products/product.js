2d2d766ba97c774097c9b7c750a9a5cbda8aea36
Kevin Castro <cas20057@byui.edu>
2023-07-13 22:49:09 -0600

    fetched all products and modified fake data for unique id

--- products/product.js
+++ products/product.js
@@ -4,3 +4,8 @@ loadHeader();
 
 const partNumberParam = getParam('partNumber');
 console.log(partNumberParam);
+
+// Fetching products from 'database' according to partNumber
+const productResponse = await fetch('../allProducts.json');
+const productData = await productResponse.json();
+const singleProduct = console.log(productData);
